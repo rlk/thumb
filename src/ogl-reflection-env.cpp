4385e20822d86e0b82d3fe7a3d5bda71931fe271
Robert Kooima <robert.kooima@gmail.com>
2009-03-05 17:08:16 +0000

    Forgot to add reflection-env module.
    git-svn-id: https://s.snth.net/svn/rlk/thumb@1623 dcf3a027-e0e4-0310-89c9-ab9954db6d00

---
+++ src/ogl-reflection-env.cpp
@@ -0,0 +1,57 @@
+//  Copyright (C) 2009 Robert Kooima
+//
+//  THUMB is free software; you can redistribute it and/or modify it under
+//  the terms of  the GNU General Public License as  published by the Free
+//  Software  Foundation;  either version 2  of the  License,  or (at your
+//  option) any later version.
+//
+//  This program  is distributed in the  hope that it will  be useful, but
+//  WITHOUT   ANY  WARRANTY;   without  even   the  implied   warranty  of
+//  MERCHANTABILITY  or FITNESS  FOR A  PARTICULAR PURPOSE.   See  the GNU
+//  General Public License for more details.
+
+#include <cassert>
+
+#include "app-glob.hpp"
+#include "app-conf.hpp"
+#include "ogl-frame.hpp"
+#include "ogl-binding.hpp"
+#include "ogl-reflection-env.hpp"
+
+//-----------------------------------------------------------------------------
+
+ogl::reflection_env::reflection_env() :
+    process("reflection-env"),
+
+    cube(::glob->new_frame(::conf->get_i("reflection_cubemap_size", 128),
+                           ::conf->get_i("reflection_cubemap_size", 128),
+                           GL_TEXTURE_CUBE_MAP,
+                           GL_RGBA16F_ARB, true, false, false))
+{
+    init();
+}
+
+ogl::reflection_env::~reflection_env()
+{
+    assert(cube);
+
+    ::glob->free_frame(cube);
+}
+
+//-----------------------------------------------------------------------------
+
+void ogl::reflection_env::draw(const ogl::binding *calc) const
+{
+    assert(cube);
+
+    proc_cube(calc, cube);
+}
+
+void ogl::reflection_env::bind(GLenum unit) const
+{
+    assert(cube);
+
+    cube->bind_color(unit);
+}
+
+//-----------------------------------------------------------------------------
---
+++ src/ogl-reflection-env.hpp
@@ -0,0 +1,47 @@
+//  Copyright (C) 2009 Robert Kooima
+//
+//  THUMB is free software; you can redistribute it and/or modify it under
+//  the terms of  the GNU General Public License as  published by the Free
+//  Software  Foundation;  either version 2  of the  License,  or (at your
+//  option) any later version.
+//
+//  This program  is distributed in the  hope that it will  be useful, but
+//  WITHOUT   ANY  WARRANTY;   without  even   the  implied   warranty  of
+//  MERCHANTABILITY  or FITNESS  FOR A  PARTICULAR PURPOSE.   See  the GNU
+//  General Public License for more details.
+
+#ifndef OGL_REFLECTION_ENV_HPP
+#define OGL_REFLECTION_ENV_HPP
+
+#include "ogl-process.hpp"
+
+//-----------------------------------------------------------------------------
+
+namespace ogl
+{
+    class frame;
+    class binding;
+}
+
+//-----------------------------------------------------------------------------
+
+namespace ogl
+{
+    class reflection_env : public process
+    {
+        ogl::frame *cube;
+
+    public:
+
+        reflection_env();
+       ~reflection_env();
+
+        void draw(const ogl::binding *) const;
+
+        void bind(GLenum) const;
+    };
+}
+
+//-----------------------------------------------------------------------------
+
+#endif
