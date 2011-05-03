2e14d65ea60090cef3035ada97ea092ffe857d83
Robert Kooima <robert.kooima@gmail.com>
2011-05-03 21:52:12 +0000

    Adding a global header and main page documentation.
    git-svn-id: https://s.snth.net/svn/rlk/thumb@2631 dcf3a027-e0e4-0310-89c9-ab9954db6d00

---
+++ include/thumb.hpp
@@ -0,0 +1,43 @@
+//  Copyright (C) 2005-2011 Robert Kooima
+//
+//  THUMB is free software; you can redistribute it and/or modify it under
+//  the terms of  the GNU General Public License as  published by the Free
+//  Software  Foundation;  either version 2  of the  License,  or (at your
+//  option) any later version.
+//
+//  This program  is distributed in the  hope that it will  be useful, but
+//  WITHOUT   ANY  WARRANTY;   without  even   the  implied   warranty  of
+//  MERCHANTABILITY  or FITNESS  FOR A  PARTICULAR PURPOSE.   See  the GNU
+//  General Public License for more details.
+
+#ifndef THUMB_HPP
+#define THUMB_HPP
+
+//-----------------------------------------------------------------------------
+
+/// \mainpage Thumb
+///
+/// Thumb is a distributed real-time rendering engine, implemented as a C++
+/// class library. It provides facilities to render 3D content on multiple
+/// networked hosts simultaneously while maintaining tight synchronization of
+/// display updates at high frame rates. Thumb defines a very flexible cluster
+/// configuration mechanism and supports a wide variety of stereo display
+/// technologies and input devices. This gives broad application portability
+/// supporting high-resolution tiled displays, immersive virtual reality
+/// environments, and full dome installations, as well as common PC desktops.
+///
+/// Thumb provides a high-performance static geometry optimizer with support
+/// for programmable materials. It links the Open Dynamics Engine to a simple
+/// 3D scene graph, giving physically responsive interaction. It implements a
+/// basic 3D GUI with high-quality internationalized text rendering and a
+/// variety of common widgets. Bringing all of these features together, Thumb
+/// implements a built-in real-time 3D scene editor with unlimited undo that
+/// naturally embeds within all applications.
+///
+/// &copy; 2005-2011 Robert Kooima <robert.kooima@gmail.com>
+///
+
+
+//-----------------------------------------------------------------------------
+
+#endif
