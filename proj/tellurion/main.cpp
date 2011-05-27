6290ed83216290466762d972bfa54aca1fa6df56
Robert Kooima <robert.kooima@gmail.com>
2011-05-27 20:07:35 +0000

    Forgot to add the new tellurion stuff.
    git-svn-id: https://s.snth.net/svn/rlk/thumb@2655 dcf3a027-e0e4-0310-89c9-ab9954db6d00

---
+++ proj/tellurion/main.cpp
@@ -0,0 +1,43 @@
+//  Copyright (C) 2005 Robert Kooima
+//
+//  THUMB is free software; you can redistribute it and/or modify it under
+//  the terms of  the GNU General Public License as  published by the Free
+//  Software  Foundation;  either version 2  of the  License,  or (at your
+//  option) any later version.
+//
+//  This program  is distributed in the  hope that it will  be useful, but
+//  WITHOUT   ANY  WARRANTY;   without  even   the  implied   warranty  of
+//  MERCHANTABILITY  or FITNESS  FOR A  PARTICULAR PURPOSE.   See  the GNU
+//  General Public License for more details.
+
+#include <SDL.h>
+
+#include <stdexcept>
+#include <iostream>
+
+#include <app-prog.hpp>
+#include <app-default.hpp>
+
+#include "tellurion.hpp"
+
+//-----------------------------------------------------------------------------
+
+int main(int argc, char *argv[])
+{
+    try
+    {
+        app::prog *P;
+
+        P = new tellurion(std::string(argc > 1 ? argv[1] : DEFAULT_TAG));
+        P->run();
+
+        delete P;
+    }
+    catch (std::exception& e)
+    {
+        std::cerr << "Exception: " << e.what() << std::endl;
+    }
+    return 0;
+}
+
+//-----------------------------------------------------------------------------
