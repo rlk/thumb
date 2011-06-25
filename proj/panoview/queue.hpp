6279067c6ef7dcb06072984ea2d48ff7bd0b849e
Robert Kooima <robert.kooima@gmail.com>
2011-06-25 14:33:31 +0000

    Sketching queue implementation.
    git-svn-id: https://s.snth.net/svn/rlk/thumb@2731 dcf3a027-e0e4-0310-89c9-ab9954db6d00

--- proj/panoview/queue.hpp
+++ proj/panoview/queue.hpp
@@ -18,6 +18,11 @@
 // FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
 // DEALINGS IN THE SOFTWARE.
 
-#include "tree.hpp"
+#ifndef QUEUE_HPP
+#define QUEUE_HPP
 
 //------------------------------------------------------------------------------
+
+//------------------------------------------------------------------------------
+
+#endif
