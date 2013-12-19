15a7bc6beb5d073bcb11ada49bc6270d99f7cefc
Robert Kooima <robert.kooima@gmail.com>
2013-12-19 14:54:51 -0600

    Change tilt key to something that works on OSX

--- src/app-prog.cpp
+++ src/app-prog.cpp
@@ -184,7 +184,7 @@ app::prog::prog(const std::string& exe,
 
     // Configure some application-level key bindings.
 
-    key_init = ::conf->get_i("key_init", SDL_SCANCODE_F10);
+    key_init = ::conf->get_i("key_init", SDL_SCANCODE_F9);
     key_snap = ::conf->get_i("key_snap", SDL_SCANCODE_F13);
 
     SDL_StopTextInput();
