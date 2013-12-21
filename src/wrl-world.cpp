00565f9ad5135f19f56f12744695e2971e8b883d
Robert Kooima <robert.kooima@gmail.com>
2013-12-21 16:28:45 -0600

    Fix unused lights not being zeroed

--- src/wrl-world.cpp
+++ src/wrl-world.cpp
@@ -1022,18 +1022,16 @@ void wrl::world::lite(int frusc, const app::frustum *const *frusv)
 
     atom_set::const_iterator a;
 
-    for (a = all.begin(); a != all.end(); ++a)
+    for (a = all.begin(); a != all.end() && (*a)->priority() < 0; ++a)
     {
         const mat4 T = (*a)->get_fill()->get_world_transform();
         const vec3 p = wvector(T);
         const vec3 v = yvector(T);
 
-        switch ((*a)->priority())
-        {
-            case -1: light += s_light(p, v, frusc, frusv, light, bb, *a); break;
-            case -2: light += d_light(p, v, frusc, frusv, light, bb, *a); break;
-            default: return;
-        }
+        if ((*a)->priority() == -1)
+            light += s_light(p, v, frusc, frusv, light, bb, *a);
+        if ((*a)->priority() == -2)
+            light += d_light(p, v, frusc, frusv, light, bb, *a);
     }
 
     // Zero the unused lights.
