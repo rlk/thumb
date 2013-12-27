ffb49aab5af5f38c1a5c447bca5152fd82a7e093
Robert Kooima <robert.kooima@gmail.com>
2013-12-27 09:16:11 -0600

    Remove the obsolete frustum API reference

--- include/app-frustum.hpp
+++ include/app-frustum.hpp
@@ -36,18 +36,10 @@ namespace app
 // far distances are decided. Finally, a final projection matrix may be found
 // and used for rendering.
 
-// set_eye
-// set_view
-//     cache_planes
-// do visibility
-// set_bound
-//     cache_points
-// do rendering
-
 namespace app
 {
     //-------------------------------------------------------------------------
-#if 1
+
     class frustum
     {
     public:
@@ -173,110 +165,10 @@ namespace app
 
     //-------------------------------------------------------------------------
 
-#else
-
-    class frustum
-    {
-    public:
-
-        frustum(app::node node);
-        frustum(const frustum& that);
-        frustum(const ogl::aabb&, const vec3&);
-
-        // View state mutators
-
-        void set_projection(const mat4& M);
-        void set_eye (const vec3& p);
-        void set_transform (const mat4& M);
-        void set_distances (const ogl::aabb& bound);
-
-        // Queries.
-
-        const vec3 get_user_pos()    const;
-        const vec3 get_view_pos()    const;
-        const vec3 get_disp_pos()    const;
-        const mat4 get_transform() const;
-
-        double get_w()          const;
-        double get_h()          const;
-
-        const vec4 *get_planes() const { return view_planes; }
-        const vec3 *get_points() const { return view_points; }
-
-        double get_n_dist() const { return n_dist; }
-        double get_f_dist() const { return f_dist; }
-
-        // Parallel-split handlers
-
-        double    get_split_c    (int, int) const;
-        double    get_split_k    (int, int) const;
-        double    get_split_z    (int, int) const;
-        ogl::aabb get_split_bound(int, int) const;
-
-        // Event handlers
-
-        bool pointer_to_3D(event *, double,  double)  const;
-        bool pointer_to_2D(event *, double&, double&) const;
-        bool process_event(event *);
-
-        // Perspective projection application
-
-        void load_transform() const;
-        void apply_overlay()  const;
-
-    private:
-
-        // Utility functions
-
-        void get_calibration(double&, double&, double&, double&,
-                             double&, double&, double&, double&);
-        void set_calibration(double,  double,  double,  double,
-                             double,  double,  double,  double);
-        mat4 mat_calibration();
-
-        void calc_corner_4(vec3&,
-                           vec3&,
-                           vec3&,
-                           vec3&, double, double);
-        vec3 calc_corner_1(const vec3&,
-                           const vec3&,
-                           const vec3&);
-        void calc_calibrated();
-        void calc_basis();
-
-        // Serialization node
-
-        app::node node;
-
-        // Current view point
-
-        vec3 user_pos;
-        vec3 view_pos;
-
-        // Frustum bounding planes and points
-
-        vec3 user_points[4];
-        vec3 view_points[8];
-        vec4 view_planes[6];  // N L R B T
-
-        // User-space basis for the display coordinate system
-
-        mat3 user_basis;
-
-        // Projection transform
-
-        mat4 P;
-
-        double n_dist;
-        double f_dist;
-    };
-#endif
     typedef std::vector<frustum *>                 frustum_v;
     typedef std::vector<frustum *>::const_iterator frustum_i;
 
     //-------------------------------------------------------------------------
 }
-
-//-----------------------------------------------------------------------------
 
 #endif
